f90446057a45ca20906ff0a5a9a01218edf330b4
David Link <dvlink@gmail.com>
2023-09-11 03:54:52 +0000

    Switching from Postgres to MySQL

--- sql/create_pieces.sql
+++ sql/create_pieces.sql
@@ -1,40 +1,39 @@
-drop table if exists pieces;
-
 create table pieces (
-  id          serial primary key,
-  medium_id   integer not null,
-  code        varchar(20) not null,
-  name        varchar(100) not null,
-  description varchar(250),
-  material    varchar(100),
-  created     timestamptz,
-  created_year integer,
-  length      integer,
-  width       integer,
-  height      integer,
-  dim_uom     varchar(5),
-  weight      integer,
-  weight_uom  varchar(5),
-  notes       varchar(250),
-  r_created   timestamptz default current_timestamp,
-  r_updated   timestamptz not null,
+  id           integer unsigned not null auto_increment primary key,  
+  medium_id    integer unsigned not null,
+  code         varchar(20) not null,
+  name         varchar(100) not null,
+  description  varchar(250),
+  material     varchar(100),
+  created      datetime,
+  created_year integer unsigned,
+  length       integer unsigned,
+  width        integer unsigned,
+  height       integer unsigned,
+  dim_uom      varchar(5),
+  weight       integer unsigned,
+  weight_uom   varchar(5),
+  notes        varchar(250),
 
-  unique(code)
-);
-
-alter table pieces add constraint fk_medium_id
-   foreign key (medium_id) references mediums (id)
+  r_created    datetime     null,
+  r_updated    timestamp    not null
+    default current_timestamp on update current_timestamp,
+ 
+  unique key (code)
+)
+engine InnoDB default charset=utf8;
 ;
 
-create trigger pieces_set_created_and_updated
-   before insert or update on pieces
-      for each row
-         execute function set_created_and_updated()
+create trigger pieces_create before insert on pieces
+   for each row set new.r_created = now()
 ;
 
 insert into pieces
-   (medium_id, code, name, material, created_year, length, width, height, dim_uom)
+   (medium_id, code, name, material, created_year, length, width,
+   height, dim_uom)
 values
-  (1, 'gardian', 'Guardian', 'Welded Aluminum, Powder Coated', '2023', 2, 2, 6, 'ft');
+  (1, 'gardian', 'Guardian', 'Welded Aluminum, Powder Coated',
+  '2023', 2, 2, 6, 'ft')
+;
 
 select * from pieces;
