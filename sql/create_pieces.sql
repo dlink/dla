8d7ed43ba4d63d68802b8adbd05382179c8300a2
David Link <dvlink@gmail.com>
2023-09-11 01:50:18 +0000

    create_pieces working

--- sql/create_pieces.sql
+++ sql/create_pieces.sql
@@ -1,28 +1,29 @@
+drop table if exists pieces;
+
 create table pieces (
-  id          serial primary key
-  mediums_id  integer not null,
+  id          serial primary key,
+  medium_id   integer not null,
   code        varchar(20) not null,
   name        varchar(100) not null,
   description varchar(250),
   material    varchar(100),
   created     timestamptz not null,
-  length      integer unsigned,
-  width       integer unsigned,
-  height      integer unsigned,
+  length      integer,
+  width       integer,
+  height      integer,
   dim_uom     varchar(5),
-  weight      integer unsigned,
+  weight      integer,
   weight_uom  varchar(5),
   notes       varchar(250),
-  r_created   timestamptz not null,
+  r_created   timestamptz default current_timestamp,
   r_updated   timestamptz not null,
 
-  unique key code (code),
-  constraint mediums_id foreign key (mediums_id) references mediums (id),
+  unique(code)
 );
 
--- alter table pieces add constraint fk_mediums_id
---    foreign key (mediums_id) references mediums (id)
--- ;
+alter table pieces add constraint fk_medium_id
+   foreign key (medium_id) references mediums (id)
+;
 
 create trigger pieces_set_created_and_updated
    before insert or update on pieces
@@ -30,3 +31,4 @@ create trigger pieces_set_created_and_updated
          execute function set_created_and_updated()
 ;
 
+insert into pieces (medium_id, code, name, created) values (1, 'test', 'test', '2023-09-01');
