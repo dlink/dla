eac7a4f1a443c13dc54770faa822665e43bf371c
David Link <dvlink@gmail.com>
2023-09-11 01:41:39 +0000

    Getting create scripts, triggers and functions to work

--- sql/create_functions.sql
+++ sql/create_functions.sql
@@ -1,13 +1,16 @@
 -- set r_created on inserts
 -- set r_updated on all updates
 
+drop function if exists set_created_and_updated cascade;
+
 create or replace function set_created_and_updated()
 returns trigger as $$
 begin
-  if tg_op = 'insert' then
+  if tg_op = 'INSERT' then
     new.r_created = now();
   end if;
   new.r_updated = now();
   return new;
 end;
-$$ language plpgsql;
+$$ language plpgsql
+;
--- sql/create_mediums.sql
+++ sql/create_mediums.sql
@@ -1,2 +1,21 @@
+drop table if exists mediums;
+
 create table mediums (
+  id          serial primary key,
+  code        varchar(20) not null,
+  name        varchar(100) not null,
+  r_created   timestamptz default current_timestamp,
+  r_updated   timestamptz,
+
+  unique(code)
 );
+create trigger mediums_updates
+   before insert or update on mediums
+      for each row
+         execute function set_created_and_updated()
+;
+insert into mediums (code, name) values ('sculpt', 'Sculpture');
+insert into mediums (code, name) values ('paint', 'Painting');
+insert into mediums (code, name) values ('draw', 'Drawing');
+insert into mediums (code, name) values ('shirt', 'T-Shirt');
+insert into mediums (code, name) values ('shirt', 'T-Shirt');
