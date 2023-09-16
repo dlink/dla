e76ad86624010bfc3aedf0b889f8b21a5bc2d622
David Link <dvlink@gmail.com>
2023-09-16 14:51:24 +0000

    Format decimal dimensions: 1.50 -> 1-1/2

--- lib/pieces.py
+++ lib/pieces.py
@@ -46,10 +46,33 @@ class Piece(DataRecord):
     def dimensions(self):
         dimensions = ''
         if self.length and self.width and self.height:
-            dimensions = f'{self.length} x {self.width} x {self.height}'
+            dimensions = \
+                f'{dimf(self.length)} x {dimf(self.width)} x ' \
+                f'{dimf(self.height)}'
             if self.dim_uom:
                 dimensions = f'{dimensions} {self.dim_uom}'
         return dimensions
+
+def dimf(dim):
+    '''format Decimal dimension number
+         match these:
+           1.00 -> 1
+           1.25 -> 1-1/4
+           1.50 -> 1-1/2
+           1.75 -> 1-3/4
+         else:
+           1.30 -> 1.30
+    '''
+    dim_int = int(dim)
+    if dim_int == dim:
+        return dim_int
+    elif dim_int + 0.25 == dim:
+        return f'{dim_int}-1/4'
+    elif dim_int + 0.5 == dim:
+        return f'{dim_int}-1/2'
+    elif dim_int + 0.75 == dim:
+        return f'{dim_int}-3/4'
+    return dim
 
 class PiecesCLIError(Exception): pass
 
