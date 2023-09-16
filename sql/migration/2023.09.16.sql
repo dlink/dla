4fe3481d1c152145b3d22c1940ef727a55325dea
David Link <dvlink@gmail.com>
2023-09-16 14:51:52 +0000

    Data model: change piece dimension fields from int to decimal

---
+++ sql/migration/2023.09.16.sql
@@ -0,0 +1,6 @@
+alter table pieces
+   change column length length decimal(6,2),
+   change column width width decimal(6,2),
+   change column height height decimal(6,2),
+   change column weight weight decimal(6,2)
+;
