fb5994d2923dfe1f7bbf4478dba58b10a5076f62
David Link <dvlink@gmail.com>
2023-10-10 00:11:26 +0000

    Images: added resize_pad (to 4:3 aspect ratio) and rotate

--- lib/images.py
+++ lib/images.py
@@ -5,6 +5,8 @@ from PIL import Image as Pil
 
 from vlib.utils import lazyproperty, validate_num_args
 
+import env
+
 class Images():
     HIRES = 1200
     SIZES = {'tiny': 50,
@@ -21,6 +23,8 @@ class Image():
         self.dir, self.filename, self.filename_wo, self.ext = \
             self.fileparts
         self.action_statuses = []
+        self.env = env.getInstance()
+        self.verbose = self.env.verbose
 
     @lazyproperty
     def img(self):
@@ -47,19 +51,78 @@ class Image():
         resized_img = self.img.resize((width, height))
 
         if inplace:
-            # make backup of file
-            done = bk = 0
-            while not done:
-                bk += 1
-                dir = self.dir + '/' if self.dir else ''
-                bk_filepath = f'{self.dir}{self.filename_wo}_bk{bk}{self.ext}'
-                if not os.path.exists(bk_filepath):
-                    done = 1
-            self.img.save(bk_filepath)
+            self.makeBackup()
             outputfile = self.filepath
 
         # write file
         resized_img.save(outputfile)
+
+    def resize_pad(self, width, outputfile=None, inplace=False):
+        '''Resize an image to to have a 4:3 aspect ratio with given width
+           Pad image with white space if necessary.
+        '''
+        if not inplace and not outputfile:
+            raise ImageError('Must specify outputfile or pass inplace=True')
+
+        taspect_ratio = 4 /3
+
+        width = input_width = int(width)
+        height = calc_height = int(width * taspect_ratio)
+
+        padded_img = Pil.new('RGB', (width, height), 'white')
+
+        owidth, oheight = self.img.size
+        oaspect_ratio = owidth / oheight
+        if oaspect_ratio < taspect_ratio:
+            width = int(height * oaspect_ratio)
+        else:
+            height = int(width / oaspect_ratio)
+        resized_img = self.img.resize((width, height),
+                                      Pil.Resampling.LANCZOS)
+        paste_pos = ((padded_img.width - resized_img.width) // 2,
+                     (padded_img.height - resized_img.height) // 2)
+        padded_img.paste(resized_img, paste_pos)
+
+        if self.verbose:
+            print('input width:', input_width)
+            print('calc height:', calc_height)
+            print('orig width, height:', owidth, oheight)
+            print('taspect_ratio:', round(taspect_ratio, 4))
+            print('oaspect_ratio:', round(oaspect_ratio, 4))
+            if width != input_width:
+                print('changed width:', width)
+            else:
+                 print('changed height:', height)
+            print('new width, height:', width, height)
+            print('final width, height:', padded_img.width, padded_img.height)
+            print('paste pos:', paste_pos)
+
+        if inplace:
+            self.makeBackup()
+            outputfile = self.filepath
+
+        # write file
+        padded_img.save(outputfile)
+
+    def rotate(self, outputfile=None, inplace=False):
+        if not inplace and not outputfile:
+            raise ImageError('Must specify outputfile or pass inplace=True')
+
+        rotated_img = self.img.rotate(-90, expand=True)
+        if inplace:
+            self.makeBackup()
+            outputfile = self.filepath
+        rotated_img.save(outputfile)
+
+    def makeBackup(self):
+        done = bk = 0
+        while not done:
+            bk += 1
+            dir = self.dir + '/' if self.dir else ''
+            bk_filepath = f'{self.dir}{self.filename_wo}_bk{bk}{self.ext}'
+            if not os.path.exists(bk_filepath):
+                done = 1
+        self.img.save(bk_filepath)
 
     def to_png(self):
         '''Create a png file'''
@@ -124,6 +187,9 @@ class ImagesCLI(object):
        Used for discovery
     '''
 
+    def __init__(self):
+        self.env = env.getInstance()
+
     def run(self):
         '''Set up Command Line (CLI) commands and options
            for Images Module
@@ -133,6 +199,8 @@ class ImagesCLI(object):
             'resize <filepath> <width> <height> [<output_file>]',
             'resize_by_width <filepath> <width> [<output_file>]',
             'resize_by_height <filepath> <height> [<output_file>]',
+            'resize_pad <filepath> <width> [<output_file>] # 4:3',
+            'rotate <filepath> [<output_file>] # clockwise',
             'stat <filepath>',
             'to_png <filepath>',
         ]
@@ -145,6 +213,9 @@ class ImagesCLI(object):
         '''
         args = list(args)
         cmd = args.pop(0)
+
+        if self.cli.hasoption.get('v'):
+            self.env.verbose = 1
 
         if cmd == 'resize':
             num_args = 3
@@ -161,6 +232,7 @@ class ImagesCLI(object):
                 outputfile = args.pop(0)
                 inplace = 0
             return Image(filepath).resize(width, height, outputfile, inplace)
+
         if cmd in ('resize_by_width', 'resize_by_height'):
             num_args = 2
             if self.cli.hasoption.get('i'):
@@ -180,6 +252,33 @@ class ImagesCLI(object):
             else:
                 return Image(filepath).resize(
                     height=pixels, outputfile=outputfile, inplace=inplace)
+
+        if cmd == 'resize_pad':
+            num_args = 2
+            if self.cli.hasoption.get('i'):
+                num_args = 1
+            validate_num_args('resize_pad', num_args, args)
+            filepath = args.pop(0)
+            width = args.pop(0)
+            if self.cli.hasoption.get('i'):
+                outputfile = None
+                inplace = 1
+            else:
+                outputfile = args.pop(0)
+                inplace = 0
+            return Image(filepath).resize_pad(width, outputfile=outputfile,
+                                              inplace=inplace)
+
+        elif cmd == 'rotate':
+            validate_num_args('rotate', 1, args)
+            filepath = args.pop(0)
+            if self.cli.hasoption.get('i'):
+                outputfile = None
+                inplace = 1
+            else:
+                outputfile = args.pop(0)
+                inplace = 0
+            return Image(filepath).rotate(outputfile, inplace=inplace)
 
         elif cmd == 'stat':
             validate_num_args('stat', 1, args)
