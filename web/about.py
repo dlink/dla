fff40a22196838d1658b324db105220d94b767cd
David Link <dvlink@gmail.com>
2023-10-19 23:55:52 +0000

    bug fix

--- web/about.py
+++ web/about.py
@@ -5,9 +5,6 @@ class AboutPage(BasePage):
 
     def __init__(self):
         BasePage.__init__(self, 'David Link About Page')
-        self.style_sheets.extend([
-             self.versionize('css/main.css'),
-         ])
 
     def getPageContent(self):
         return '<h3 style="margin: 20px">About Page</h2>'
--- web/contact.py
+++ web/contact.py
@@ -5,9 +5,6 @@ class ContactPage(BasePage):
 
     def __init__(self):
         BasePage.__init__(self, 'David Link Contact Page')
-        self.style_sheets.extend([
-             self.versionize('css/main.css'),
-         ])
 
     def getPageContent(self):
         return '<h3 style="margin: 20px">Contact Page</h2>'
