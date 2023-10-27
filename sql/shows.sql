1e70d66e960a70f0525cf9b81c9f432a67bb74f5
David Link <dvlink@gmail.com>
2023-10-27 00:59:05 +0000

    Data Model: shows: added location and year, removed unique key on name

--- sql/shows.sql
+++ sql/shows.sql
@@ -1,21 +1,21 @@
 
 create table shows (
    id            integer unsigned not null auto_increment primary key,
-   code          varchar(35)      not null,
+   code          varchar(60)      not null,
    name          varchar(255)     not null,
+   location      varchar(50)      ,
    description   text             ,
    contact_id    integer unsigned ,
-   active        boolean          not null,
    start_date    date,
    end_date      date,
    opening       date,
+   year          integer unsigned ,
 
    r_created     datetime         null,
    r_updated     timestamp        not null
 	default current_timestamp on update current_timestamp,
 
-   unique key(code),
-   unique key(name)
+   unique key(code)
 
 ) engine InnoDB default charset=utf8;
 ;
