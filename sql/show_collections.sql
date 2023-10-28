962bdfbcd9623e48459de04642cf63e1f0b8308a
David Link <dvlink@gmail.com>
2023-10-28 20:44:58 +0000

    adding show_commision script

---
+++ sql/show_collections.sql
@@ -0,0 +1,10 @@
+select
+   s.sale_date,
+   concat_ws(', ', o.city, o.state) as city,
+   concat_ws('-', p.name, p.version) as piece
+   
+from
+   sales s
+   join pieces p on s.piece_id = p.id
+   join contacts o on s.owner_id = o.id
+;
